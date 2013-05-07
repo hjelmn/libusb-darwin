MotorolaMobilityLLC/external-libusb

All: Avoid polluting errors reported on device disconnect
* Makes libusb_cancel_transfer not log an error when the cancel fails
  with LIBUSB_ERROR_NO_DEVICE, so that apps can properly clean things
  up on device disconnect without flooding the console with these errors.
* Also, some devices (Cypress FX) may switch VID:PID on the fly during
  firmware upload => reduce severity of the Windows message when that happens.

libusb/os/windows_usb.c
@@ -2877,7 +2877,7 @@ static int winusbx_submit_control_transfer(int sub_api, struct usbi_transfer *it
 	} else {
 		if (!WinUSBX[sub_api].ControlTransfer(wfd.handle, *setup, transfer->buffer + LIBUSB_CONTROL_SETUP_SIZE, size, NULL, wfd.overlapped)) {
 			if(GetLastError() != ERROR_IO_PENDING) {
-				usbi_err(ctx, "ControlTransfer failed: %s", windows_error_str(0));
+				usbi_warn(ctx, "ControlTransfer failed: %s", windows_error_str(0));
 				usbi_free_fd(wfd.fd);
 				return LIBUSB_ERROR_IO;
 			}

hjelmn/libusb-darwin

Conflicts:
	libusb/os/windows_usb.c
	libusb/version_nano.h

libusb/os/windows_usb.c
@@ -2595,7 +2595,7 @@ static int winusb_submit_control_transfer(struct usbi_transfer *itransfer)
 	} else {
 		if (!WinUsb_ControlTransfer(wfd.handle, *setup, transfer->buffer + LIBUSB_CONTROL_SETUP_SIZE, size, NULL, wfd.overlapped)) {
 			if(GetLastError() != ERROR_IO_PENDING) {
-				usbi_err(ctx, "WinUsb_ControlTransfer failed: %s", windows_error_str(0));
+				usbi_warn(ctx, "WinUsb_ControlTransfer failed: %s", windows_error_str(0));
 				usbi_free_fd(wfd.fd);
 				return LIBUSB_ERROR_IO;
 			}